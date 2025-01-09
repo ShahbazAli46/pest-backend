ca48e1719489935c6b233831b5f86df5f71305c4
hamza2236 <hamza502m@gmail.com>
2025-01-09 18:32:31 +0500

    change in  salary detail api

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -17,22 +17,21 @@ class SalaryController extends Controller
             }
             
             $employee_salary = $employee_salary_query->get();
-            
-            $data['total_basic_salary'] = $employee_salary->sum('basic_salary'); 
-            $data['total_allowance'] = $employee_salary->sum('allowance'); 
-            $data['total_other'] = $employee_salary->sum('other'); 
-            $data['total_salary'] = $employee_salary->sum('total_salary'); 
-            $data['total_fines'] = $employee_salary->sum('total_fines'); 
-            $data['total_adv_paid'] = $employee_salary->sum('adv_paid'); 
-            $data['total_payable_salary'] = $employee_salary->sum('payable_salary'); 
-            $data['total_paid_salary'] = $employee_salary->sum('paid_salary'); 
-            $data['total_adv_received'] = $employee_salary->sum('adv_received'); 
-            $data['total_remaining_salary'] = $employee_salary->sum('remaining_salary'); 
+            $data[]=['name'=>'Total Basic Salary','value'=>$employee_salary->sum('basic_salary')];
+            $data[]=['name'=>'Total Allowance','value'=>$employee_salary->sum('allowance')];
+            $data[]=['name'=>'Total Other','value'=>$employee_salary->sum('other')];
+            $data[]=['name'=>'Total Salary','value'=>$employee_salary->sum('total_salary')];
+            $data[]=['name'=>'Total Fines','value'=>$employee_salary->sum('total_fines')];
+            $data[]=['name'=>'Adv Paid','value'=>$employee_salary->sum('adv_paid')];
+            $data[]=['name'=>'Payable Salary','value'=>$employee_salary->sum('payable_salary')];
+            $data[]=['name'=>'Paid Salary','value'=>$employee_salary->sum('paid_salary')];
+            $data[]=['name'=>'Adv Received','value'=>$employee_salary->sum('adv_received')];
+            $data[]=['name'=>'Remaining Salary','value'=>$employee_salary->sum('remaining_salary')];
+
+            $data[]=['name'=>'Total WPS','value'=>$employee_salary->where('transection_type', 'wps')->where('status','paid')->sum('paid_salary')];
+            $data[]=['name'=>'Total Cash','value'=>$employee_salary->where('transection_type', 'wps')->where('status','paid')->sum('total_cash')];
 
 
-            $data['total_wps'] = $employee_salary->where('transection_type', 'wps')->where('status','paid')->sum('paid_salary'); 
-            $data['total_cash'] = $employee_salary->where('transection_type', 'cash')->where('status','paid')->sum('paid_salary'); 
-            
             return response()->json(['data' => $data]);
         } catch (\Illuminate\Validation\ValidationException $e) {
             return response()->json(['status'=> 'error','message' => $e->validator->errors()->first()], 422);
